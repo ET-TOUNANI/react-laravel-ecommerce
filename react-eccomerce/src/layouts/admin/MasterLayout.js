82a53506dec65806ebcbc1f074c2f34ab69b9ac0
ET-TOUNANI <tounaniabderrahmane99@gmail.com>
2022-03-19 21:07:00 +0100

    install react-router-dom && setub folders for project

---
+++ react-eccomerce/src/layouts/admin/MasterLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+function MasterLayout() {
+  return (
+
+    <div className='sb-nav-fixed'>
+        <Navbar />
+    </div>
+  )
+}
+
+export default MasterLayout
