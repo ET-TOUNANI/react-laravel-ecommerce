b9ee15878c1050e5462c3e6c6a0a01322172a442
ET-TOUNANI <tounaniabderrahmane99@gmail.com>
2022-03-20 19:08:31 +0100

    laravel initialisaton

--- react-eccomerce/src/App.js
+++ react-eccomerce/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter,Route ,Routes } from  'react-router-dom'
 import  MasterLayout  from "./layouts/admin/MasterLayout";
 import NotFound from './NotFound'
-import Home from './Home'
+import Home from './layouts/frontend/Home'
 import Dashboard from "./components/admin/Dashboard";
 import Profile from "./components/admin/Profile";
 function App() {
@@ -11,14 +11,6 @@ function App() {
       <BrowserRouter>
             <Routes>
               <Route index element={<Home />} />
-              {/*<Route exact path="/" component={App} />
-              <Route path="/users/login" component={Login} />
-              <Route exact path="/users/register" component={Register} />
-              <Route path="/users/register/:key" component={ConfirmAddr} />
-              <Route path="/users/forgot-password" component={ForgotPassword} />
-              <Route path="/users/reset-password/:key" component={ResetPassword} />
-              <Route path="/users/profile/:username" component={UserProfile} />
-              <Route path="/chat/messages" component={Messages} />*/}
               <Route  path="/admin"  name="admin" element={<MasterLayout/>} >
                 <Route path="dashboard" element={<Dashboard />} />
                 <Route path="profile" element={<Profile />} />
