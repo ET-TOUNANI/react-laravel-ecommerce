47ed9e27398ac84dca379ba8ca9a98a760a26c18
ET-TOUNANI <tounaniabderrahmane99@gmail.com>
2022-03-20 21:34:45 +0100

    FIX ERRORS

--- react-eccomerce/src/App.js
+++ react-eccomerce/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter,Route ,Routes } from  'react-router-dom'
 import  MasterLayout  from "./layouts/admin/MasterLayout";
 import NotFound from './NotFound'
-import Home from './layouts/frontend/Home'
+import Home from './components/frontend/Home'
 import Dashboard from "./components/admin/Dashboard";
 import Profile from "./components/admin/Profile";
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <BrowserRouter>
             <Routes>
               <Route index element={<Home />} />
+
               <Route  path="/admin"  name="admin" element={<MasterLayout/>} >
                 <Route path="dashboard" element={<Dashboard />} />
                 <Route path="profile" element={<Profile />} />
---
+++ react-eccomerce/src/components/frontend/auth/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+        <button className='btn btn-danger'>hi</button>
+    </div>
+  )
+}
+
+export default Register
