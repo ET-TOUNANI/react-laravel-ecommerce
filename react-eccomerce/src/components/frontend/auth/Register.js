be7e4931e7dececf88e7cb6e57974fa3d68925ca
ET-TOUNANI <tounaniabderrahmane99@gmail.com>
2022-03-20 23:41:53 +0100

    feat add ui authentication

--- react-eccomerce/src/components/frontend/auth/Register.js
+++ react-eccomerce/src/components/frontend/auth/Register.js
@@ -4,7 +4,44 @@ function Register() {
   return (
     <div>
        <Navbar/>
-        <h2>i am register page</h2>
+        <div className="container py-5">
+          <div className="row justify-content-center ">
+            <div className="col-md-6">
+              <div className="card justify-content-center">
+                <div className="card-header">
+                  <h1>Register </h1>
+                </div>
+                <div className="card-body">
+                  <form action="">
+
+                   <div className="form-group mb-3">
+                      <label > Full Name </label>
+                      <input type="text" name="fullname"  className="form-control" required/>
+                    </div>
+
+                    <div className="form-group mb-3">
+                      <label > Last Name </label>
+                      <input type="text" name="lastname"  className="form-control" required/>
+                    </div>
+
+                    <div className="form-group mb-3">
+                      <label > email </label>
+                      <input type="email" name="email"  className="form-control" required/>
+                    </div>
+
+                    <div className="form-group mb-3">
+                      <label >password </label>
+                      <input type="password" className="form-control" name="pass" required />
+                    </div>
+                    <div className="form-group mb-3">
+                      <button type="submit" className="btn btn-primary" name="envoyer" >register</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
